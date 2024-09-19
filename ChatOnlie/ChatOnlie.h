43ee8e7cbae4940f55dd5e3ce53088266171bcbb
Fazliddinov Iskandar <exclusive.fazliddinov@gmail.com>
2024-09-19 11:20:16 +0500

    Initial Commit

---
+++ ChatOnlie/ChatOnlie.h
@@ -0,0 +1,18 @@
+//
+//  ChatOnlie.h
+//  ChatOnlie
+//
+//  Created by Fazliddinov Iskandar on 19/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatOnlie.
+FOUNDATION_EXPORT double ChatOnlieVersionNumber;
+
+//! Project version string for ChatOnlie.
+FOUNDATION_EXPORT const unsigned char ChatOnlieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatOnlie/PublicHeader.h>
+
+
